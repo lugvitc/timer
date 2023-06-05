05aecdbe311985bf1d3e6cb5219040d693b19daf
Animesh Agarwal <animeshagarwal28@gmail.com>
2023-06-05 21:07:43 +0530

    Added LUG Logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './style.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./style.css";
 
-
-const API_URL = 'https://worldtimeapi.org/api/ip';
+const API_URL = "https://worldtimeapi.org/api/ip";
 
 function App() {
   const [currentTime, setCurrentTime] = useState(null);
-  const targetTime = new Date('June 9, 2023 10:00:00').getTime();
+  const targetTime = new Date("June 9, 2023 10:00:00").getTime();
 
   useEffect(() => {
     const fetchCurrentTime = async () => {
@@ -15,24 +14,23 @@ function App() {
         const response = await axios.get(API_URL);
         setCurrentTime(response.data.unixtime * 1000);
       } catch (error) {
-        console.error('Error fetching current time:', error);
+        console.error("Error fetching current time:", error);
       }
     };
     const generateRandomConfetti = () => {
-      const confettiElements = document.getElementsByClassName('confetti');
+      const confettiElements = document.getElementsByClassName("confetti");
       for (let i = 0; i < confettiElements.length; i++) {
         const randomX = Math.random();
         const randomDelay = Math.random();
-        confettiElements[i].style.setProperty('--random-x', randomX);
-        confettiElements[i].style.setProperty('--random-delay', randomDelay);
-      }};
+        confettiElements[i].style.setProperty("--random-x", randomX);
+        confettiElements[i].style.setProperty("--random-delay", randomDelay);
+      }
+    };
 
     generateRandomConfetti();
-    
 
     const timer = setInterval(fetchCurrentTime, 1000);
     return () => clearInterval(timer);
-    
   }, []);
 
   const formatTime = (time) => {
@@ -51,35 +49,34 @@ function App() {
 
   useEffect(() => {
     const handleMouseMove = (event) => {
-      const text = document.querySelector('.moving-text');
-      text.style.left = event.pageX + 'px';
-      text.style.top = event.pageY + 'px';
+      const text = document.querySelector(".moving-text");
+      text.style.left = event.pageX + "px";
+      text.style.top = event.pageY + "px";
     };
 
-    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener("mousemove", handleMouseMove);
 
     return () => {
-      document.removeEventListener('mousemove', handleMouseMove);
+      document.removeEventListener("mousemove", handleMouseMove);
     };
   }, []);
 
   return (
     <div className="App">
-  <h1 className="title">PASSWORD</h1>
-  {currentTime ? (
-    <h2 className="timer">{formatTime(calculateTimeRemaining())}</h2>
-  ) : (
-    <h2 className="loading">Loading...</h2>
-  )}
+      <h1 className="title">PASSWORD</h1>
+      {currentTime ? (
+        <h2 className="timer">{formatTime(calculateTimeRemaining())}</h2>
+      ) : (
+        <h2 className="loading">Loading...</h2>
+      )}
 
-  <div className="confetti-container">
-    {[...Array(25)].map((_, index) => (
-      <div className="confetti" key={index}></div>
-    ))}
-  </div>
-  <div className="moving-text">capture.survive.escape</div>
-</div>
-
+      <div className="confetti-container">
+        {[...Array(25)].map((_, index) => (
+          <div className="confetti" key={index}></div>
+        ))}
+      </div>
+      <div className="moving-text">capture.survive.escape</div>
+    </div>
   );
 }
 
